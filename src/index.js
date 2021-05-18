f09de11047d378da617981263721feec950c7e9f
Kevin Boston <kbos22@gmail.com>
2021-05-18 13:04:14 -0400

    setup index, reducer, store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import NotecardReducer from ./reducers/NotecardReducer;
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, {}, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store} >
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/reducers/NotecardReducer.js
@@ -0,0 +1,13 @@
+function NotecardReducer(state = { items: [], user: {username: undefined}}){
+    console.log(action)
+    switch (action.type) {
+
+
+        default:
+            return state
+
+    }
+
+}
+
+export default NotecardReducer;
