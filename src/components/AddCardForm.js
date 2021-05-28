5c2523339371741f2c3b24bdd71b869b433dab4c
Kevin Boston <kbos22@gmail.com>
2021-05-27 21:30:27 -0400

    adding render for form

--- src/components/AddCardForm.js
+++ src/components/AddCardForm.js
@@ -22,6 +22,24 @@ class AddCardForm extends React.component {
             back: ""
         })
     }
+    determineDisabled() {
+        this.state.front === "" || this.state.back === ""
+    }
+
+    render() {
+        return(
+            <div className='AddCardForm'>
+                <form onSubmit={this.handleSubmit}>
+                    <h5>Add a Card:</h5>
+                    <label>Front: </label>
+                    <input type="text" name="front" onChange={this.handleChange} placeholder="Front of the card" value={this.state.front} /><br />
+                    <label>Back: </label>
+                    <input type="text" name="back" onChange={this.handleChange} placeholder="Back of the card" value={this.state.back} /><br />
+                    <input type="submit" disabled={this.determineDisabled}/>
+                </form>
+            </div>
+        )
+    }
 
 }
 
