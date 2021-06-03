7565148d01259342b05fc83457c5c59a35a2536d
Kevin Boston <kbos22@gmail.com>
2021-06-03 19:32:30 -0400

    adding GET_DECKS case to reducer

--- src/reducers/DeckReducer.js
+++ src/reducers/DeckReducer.js
@@ -2,6 +2,9 @@ function DeckReducer(state = {decks: []}, action) {
     console.log(action)
     switch (action.type) {
 
+        case 'GET_DECKS':
+            return {...state, decks: action.payload.decks} 
+
 
         default:
             return state
