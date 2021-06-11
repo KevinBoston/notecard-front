181b18b316b79782ff68d3174feb452a0cbd14a5
Kevin Boston <kbos22@gmail.com>
2021-06-10 23:08:31 -0400

    adding dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom'
-import {Provider} from 'react-redux'
-import {createStore, applyMiddleware, compose} from 'redux'
-import thunk from 'redux-thunk'
+import {BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import DeckReducer from './reducers/DeckReducer';
