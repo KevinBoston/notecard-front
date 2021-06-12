780b4abd42a6429774bc3f1d9d21dc28b8230c14
Kevin Boston <kbos22@gmail.com>
2021-06-12 18:31:24 -0400

    Adding AddDeck Form

--- src/components/AddDeckForm.js
+++ src/components/AddDeckForm.js
@@ -24,4 +24,22 @@ class AddDeckForm extends React.component {
         this.state.name === "" || this.state.description === ""
     }
 
+    render() {
+        return(
+            <div className='AddDeckForm'>
+                <form onSubmit={this.handleSubmit}>
+                    <h5>Add a Deck:</h5>
+                    <label>Name: </label>
+                    <input type="text" name="name" onChange={this.handleChange} placeholder="Title for the deck..." value={this.state.name} /><br />
+                    <label>Description: </label>
+                    <input type="text" name="description" onChange={this.handleChange} placeholder="Description" value={this.state.description} /><br />
+                    <label>Shold the deck be public? </label>
+                    <input type="radio" name="public" onChange={this.handleChange} value = {true} />True
+                    <input type="radio" name="public" onChange={this.handleChange} value={false} />False
+                    <input type="submit" disabled={this.determineDisabled}/>
+                </form>
+            </div>
+        )
+    }
+
 }
