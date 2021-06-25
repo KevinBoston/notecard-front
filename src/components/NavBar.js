bc1fe49ab747dac6cfc70b0b283d60793c077169
Kevin Boston <kbos22@gmail.com>
2021-06-24 21:57:25 -0400

    refining imports

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 
 
-class NavBar extends React.Component {
+class NavBar extends Component {
 
     render() {
         return(
