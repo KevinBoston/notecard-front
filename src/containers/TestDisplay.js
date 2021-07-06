07563bb0dec8ecbea4df512f7cde92b6725a468d
Kevin Boston <kbos22@gmail.com>
2021-07-06 16:02:10 -0400

    testing AddCardForm

--- src/containers/TestDisplay.js
+++ src/containers/TestDisplay.js
@@ -4,6 +4,13 @@ import AddCardForm from '../components/AddCardForm'
 
 const TestDisplay = () => {
 
+    return(
+        <div className="TestContainer">
+            <h3>Testing:</h3>
+            <AddCardForm />
+        </div>
+    )
+
 }
 
 export default TestDisplay;
