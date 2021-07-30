521c68a2d2a9f1e2c78c6b4a5f700643d9f7de73
Kevin Boston <kbos22@gmail.com>
2021-07-30 16:49:16 -0400

    refactored DeckDisplay to allow to add Deck

--- src/components/DeckDisplay.js
+++ src/components/DeckDisplay.js
@@ -4,13 +4,17 @@ import Card from './Card'
 
 const DeckDisplay = (props) => {
     console.log(props)
-   
+        const displayCards = () => {
+            if (props.deck.cards) {
+                <Card front={props.deck.cards[0].front} back={props.deck.cards[0].back} />
+            }
+        }
         return(
             <div> 
                 {props.deck.name} <br />
                 {props.deck.description} <br />
                 Public: { props.deck.public ? "True" : "False"} <br />
-                <Card front={props.deck.cards[0].front} back={props.deck.cards[0].back} />
+                {displayCards()}
             </div>
         )
 
