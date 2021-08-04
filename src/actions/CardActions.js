46eea6cbe5bf848a4eacb246f241918d2308dc26
Kevin Boston <kbos22@gmail.com>
2021-08-04 17:23:58 -0400

    commenting on predicted newCard structure

--- src/actions/CardActions.js
+++ src/actions/CardActions.js
@@ -2,6 +2,7 @@ import { react } from "react-dom"
 
 export const addCard = (newCard) => {
     console.log(newCard)
+    //Example: {front: "Foo", back: "Bar", deck_id: 3}
     return (dispatch) => {
         dispatch({
             type: 'ADD_CARD',
