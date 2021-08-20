d2828898b1533db2403c1210f0dca66778ce1b18
Kevin Boston <kbos22@gmail.com>
2021-08-20 17:59:28 -0400

    adding reducer logic for add Card

--- src/reducers/CardReducer.js
+++ src/reducers/CardReducer.js
@@ -4,7 +4,9 @@ function CardReducer(state = { decks: [], user: {username: undefined}}, action){
         case 'ADD_CARD':
             console.log(action.payload)
             //action.payload = {front:, back:, deck_id:}
-
+            let currentDeck = state.decks.filter(deck => deck.id === action.payload.deck_id);
+            currentDeck.push({front: action.payload.front, back: action.payload.back, deck_id: action.payload.deck_id})
+            return {...state, decks: [...state.decks, currentDeck]}
         default:
             return state
 
