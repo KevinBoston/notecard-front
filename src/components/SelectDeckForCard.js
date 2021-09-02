24d1e2574036206ce7d5c98b4972ee5f356a56b6
Kevin Boston <kbos22@gmail.com>
2021-09-02 17:33:16 -0400

    adding debugger for testing SelectDeck

--- src/components/SelectDeckForCard.js
+++ src/components/SelectDeckForCard.js
@@ -5,6 +5,7 @@ class SelectDeckForCard extends React.Component {
 
     render() {
         console.log(this.props)
+        debugger
         return(
             <>
                 Add card to which deck?<br />
