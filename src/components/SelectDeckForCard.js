4de92e1dd90b2055777ca190836756607f9a215b
Kevin Boston <kbos22@gmail.com>
2021-09-02 17:53:49 -0400

    building initial structure for generateOptions

--- src/components/SelectDeckForCard.js
+++ src/components/SelectDeckForCard.js
@@ -3,8 +3,18 @@ import React from 'react'
 class SelectDeckForCard extends React.Component {
     
 
+    generateOptions() {
+        const decks = this.props.decks
+        for (let i = 0; i < decks.length; i ++) {
+            console.log("Adding: " + decks[i].name)
+            return decks[i].name
+        }
+
+    }
+
     render() {
         console.log(this.props)
+
         debugger
         return(
             <>
