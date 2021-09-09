cc39100cebf50f4bf1826540f2a600927276119b
Kevin Boston <kbos22@gmail.com>
2021-09-08 20:50:02 -0400

    creating select tag

--- src/components/SelectDeckForCard.js
+++ src/components/SelectDeckForCard.js
@@ -6,10 +6,12 @@ class SelectDeckForCard extends React.Component {
     generateOptions() {
         const decks = this.props.decks
         const names = []
+        const tags = []
         for (let i = 0; i < decks.length; i ++) {
             names.push(decks[i].name)
+            tags.push(<select value={decks[i].id}>{decks[i].name}</select>)
         }
-        return names.join(',')
+        return tags
 
     }
 
