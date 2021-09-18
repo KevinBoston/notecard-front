0af71cc74b1a08255add9f8ddadb55cf24262bdb
Kevin Boston <kbos22@gmail.com>
2021-09-18 16:52:05 -0400

    passing along id

--- src/components/SelectDeckForCard.js
+++ src/components/SelectDeckForCard.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 class SelectDeckForCard extends React.Component {
     state = {
-        value: null
+        deck_id: null
 
     }
+    /*
     handleChange = e => {
         console.log(e.target)
         console.log(e)
@@ -12,16 +13,18 @@ class SelectDeckForCard extends React.Component {
         this.setState({
             [e.target.name]: e.target.value
         })
-    }
+    } */
 
     generateOptions() {
+        console.log(this.props)
+        //console.log(props)
         const decks = this.props.decks
         const names = []
-        const tags = [<option key={null}>Please select a deck:</option>]
+        const tags = [<option value={null}>Please select a deck:</option>]
         for (let i = 0; i < decks.length; i ++) {
-            console.log(decks[i].id + " " + decks[i].name)
+            //console.log(decks[i].id + " " + decks[i].name)
             names.push(decks[i].name)
-            tags.push(<option onChange={this.handleChange} value={decks[i].id}>{decks[i].name}</option>)
+            tags.push(<option onClick={(e) => this.props.handleChange(e)} name={`${decks[i].name}`} value={decks[i].id}>{decks[i].name}</option>)
         }
         return tags
 
@@ -35,7 +38,7 @@ class SelectDeckForCard extends React.Component {
             <>
             <label>
                     Add card to which deck?<br />
-                    <select value={this.state.value} onChange={this.handleChange}>
+                    <select onChange={(e) => this.props.handleChange(e)} value={this.state.deck_id}>
                         {this.generateOptions()} <br />
                     </select><br />
                 </label>
