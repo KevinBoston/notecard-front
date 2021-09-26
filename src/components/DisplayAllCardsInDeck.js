8a2f347a4bba9eb53c2c648cb5bea09cb5f24145
Kevin Boston <kbos22@gmail.com>
2021-09-26 11:56:52 -0400

    Adding DisplayAllCardsInDeck

---
+++ src/components/DisplayAllCardsInDeck.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class DisplayAllCardsInDeck extends React.Component {
+    
+
+    render() {
+        console.log(props)
+        return(
+            <>
+
+            </>
+        )
+
+    }
+}
+
+export default DisplayAllCardsInDeck
--- src/containers/TestDisplay.js
+++ src/containers/TestDisplay.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import AddCardForm from '../components/AddCardForm'
 import AddDeckForm from '../components/AddDeckForm'
+import DisplayAllCardsInDeck from '../components/DisplayAllCardsInDeck'
 
 
 const TestDisplay = (props) => {
