bbe5adc8503a5325ecaf8c1d49a5b1e290b97935
Kevin Boston <kbos22@gmail.com>
2021-10-02 15:41:43 -0400

    Splitting decks from props

--- src/components/DisplayAllCardsInDeck.js
+++ src/components/DisplayAllCardsInDeck.js
@@ -2,11 +2,18 @@ import React from 'react'
 
 class DisplayAllCardsInDeck extends React.Component {
     
+    displayCards(deck) {
+        console.log(deck.cards)
+
+    }
 
     render() {
-        console.log(this.props)
+        console.log(this.props.decks)
         return(
             <>
+                {this.props.deck.forEach(deck => {
+                    this.displayCards(deck)
+                })}
                 All Cards Incoming!
             </>
         )
